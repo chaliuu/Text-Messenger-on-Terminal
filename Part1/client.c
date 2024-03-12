4dfdc5e15f2a0f066df7f1c63f0ba89da7f7db80
Liu  Charles Chao-Hsuan <liuchar6@ug222.eecg.toronto.edu>
2024-03-11 20:14:18 -0400

    revamped command parsing to be more user friendly and error-proof

--- Part1/client.c
+++ Part1/client.c
@@ -4,14 +4,18 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <pthread.h>
+#include <errno.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdbool.h>
 
-#define MAX_NAME 32
+#define MAX_NAME 1024
 #define MAX_DATA 1024
 
 enum command_type {
-    LOGIN, LO_ACK, LO_NAK, EXIT, JOIN, JN_ACK, JN_NAK, LEAVE_SESS, NEW_SESS, NS_ACK, MESSAGE, QUERY, QU_ACK
+    LOGIN, LOGOUT, LO_ACK, LO_NAK, EXIT, JOIN, JN_ACK, JN_NAK, LEAVE_SESS, NEW_SESS, NS_ACK, MESSAGE, QUERY, QU_ACK
 };
 
 struct message {
@@ -21,35 +25,103 @@ struct message {
     unsigned char data[MAX_DATA];
 };
 
-// Global variables for client state
-int sock;
-pthread_mutex_t lock;
+int sockfd = -1;
+bool isLoggedIn;
 pthread_t recv_thread;
+char IP[128];
+char PORT[128];
+struct message msg;
 
-// Function declarations
-void *receive_handler(void *socket_desc);
+char input[MAX_DATA + MAX_NAME];
+char server_reply[MAX_DATA + MAX_NAME + 20];
+char send_buffer[MAX_DATA + MAX_NAME + 20]; // +20 for type, size, and separators
+
+void receive_handler();
 void send_message(struct message msg);
-struct message parse_command(char *input);
+int parse_command(char *input);
 void serialize_message(struct message msg, char *output);
 void deserialize_message(char *input, struct message *msg);
+int connect_to_server(char *ip, char* port);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char const *argv[]) {
+    
+    // Main loop for handling user input
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0; // Remove trailing newline
+
+        parse_command(input);
+
+        if (msg.type == LOGIN) {
+            if (connect_to_server(IP, PORT) != 0) {
+                printf("Failed to connect to the server.\n");
+                continue;
+            }
+        } else if (sockfd != -1 && isLoggedIn) {
+            // Only send messages if logged in (i.e., socket is valid)
+            send_message(msg);
+        } else if (sockfd != -1 && !(isLoggedIn)){
+            printf("Not logged in. Please log in");
+        }
+        else {
+            printf("You are not connected to a server. Please log in first.\n");
+        }
+
+        if (msg.type == EXIT) {
+            printf("Exiting...\n");
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int connect_to_server(char *ip, char * port) {
+    //references Beej's Programming Guide
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(ip, port, &hints, &servinfo)) != 0) {
+        perror("getaddrinfo\n");
+        exit(1);
+    }
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {
+        printf("talker: failed to create socket\n");
+     return 2;
+    }
+   if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        perror("talker: connect");
+        exit(1);
+    }
+    
+    freeaddrinfo(servinfo);
+    return 0;
+
+    /*
     struct sockaddr_in server;
-    char input[MAX_DATA + MAX_NAME], serializedMessage[MAX_DATA + MAX_NAME];
-    struct message msg;
 
-    // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         printf("Could not create socket\n");
         return -1;
     }
 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // Placeholder, replace with actual server IP
+    server.sin_addr.s_addr = inet_addr(ip);
     server.sin_family = AF_INET;
-    server.sin_port = htons(8888); // Placeholder, replace with actual server port
+    server.sin_port = htons(port);
 
-    // Connect to the server
     if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
         perror("Connect failed");
         return 1;
@@ -58,38 +130,19 @@ int main(int argc, char *argv[]) {
     printf("Connected to server\n");
 
     if(pthread_create(&recv_thread, NULL, receive_handler, (void*)&sock) < 0) {
-        perror("Could not create thread");
+        perror("Could not create receive thread");
         return 1;
     }
 
-    // Main loop for handling user input
-    while (1) {
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = 0; // Remove trailing newline
-
-        msg = parse_command(input);
-        serialize_message(msg, serializedMessage);
-        send_message(msg);
-
-        if (msg.type == EXIT) {
-            printf("Exiting...\n");
-            break;
-        }
-    }
-
-    pthread_join(recv_thread, NULL);
-    close(sock);
-
     return 0;
+    */
 }
 
-void *receive_handler(void *socket_desc) {
-    int sock = *(int*)socket_desc;
-    char server_reply[MAX_DATA + MAX_NAME];
+void receive_handler() {
     struct message msg;
 
     while(1) {
-        if(recv(sock, server_reply, sizeof(server_reply), 0) > 0) {
+        if(recv(sockfd, server_reply, sizeof(server_reply), 0) > 0) {
             deserialize_message(server_reply, &msg);
             // Handle different types of messages here, for simplicity we just print
             printf("Received: %s\n", msg.data);
@@ -99,23 +152,22 @@ void *receive_handler(void *socket_desc) {
 
 // Sends a message to the server
 void send_message(struct message msg) {
-    char buffer[MAX_DATA + MAX_NAME + 20]; // +20 for type, size, and separators
-    serialize_message(msg, buffer);
-    if(send(sock, buffer, strlen(buffer), 0) < 0) {
+    serialize_message(msg, send_buffer);
+    if(send(sockfd, send_buffer, strlen(send_buffer), 0) < 0) {
         puts("Send failed");
     }
 }
 
 // Parses user input into a message structure
-struct message parse_command(char *input) {
-    struct message msg;
+int parse_command(char *input) {
     memset(&msg, 0, sizeof(msg)); // Initialize the message structure
 
     if(strncmp(input, "/login", 6) == 0) {
         msg.type = LOGIN;
-        sscanf(input, "/login %s %s %s %d", msg.source, msg.data, &msg.size); // Assuming IP and port are in msg.data separated by space
+        sscanf(input, "/login %s %s %s %s", msg.source, msg.data, IP, PORT); 
     } else if(strcmp(input, "/logout") == 0) {
-        msg.type = EXIT;
+        isLoggedIn = false;
+        msg.type = LOGOUT;
     } else if(strncmp(input, "/joinsession", 12) == 0) {
         msg.type = JOIN;
         sscanf(input, "/joinsession %s", msg.data);
@@ -130,11 +182,11 @@ struct message parse_command(char *input) {
         msg.type = EXIT;
     } else {
         msg.type = MESSAGE;
-        strncpy(msg.data, input, MAX_DATA);
+        strncpy((char *)msg.data, input, MAX_DATA);
     }
 
-    msg.size = strlen(msg.data); // Set the size of the data
-    return msg;
+    msg.size = strlen((char *)msg.data); // Set the size of the data
+    return 0;
 }
 
 // Serializes a message structure into a string
